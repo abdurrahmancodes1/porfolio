ddb8f843d1dbd6e06bb0e8de7cd4a0d9e3d6e6ac
Abdur Rahman <ar7862204@gmail.com>
2025-12-01 15:25:36 +0530

    Build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/portfolio/", // correct for project repo
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: "dist", // build into docs/ for GitHub Pages
+    outDir: "docs", // <- important change
   },
 });
